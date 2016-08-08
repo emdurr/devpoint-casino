8d1415e1508f48e1277ce8ded8370b2e09e0a315
Eric Durr <emdurr@gmail.com>
2016-08-08 10:07:19 -0600

    attr_accessor player

--- casino.rb
+++ casino.rb
@@ -1,10 +1,14 @@
 require_relative 'player.rb'
 
 class Casino
-  # Make sure you use attr_accessor
+  attr_accessor :player
 
   def initialize
     puts "Welcome to the Casino!"
+    # for now 1 player
+    # we should be able to add as many players
+    # we should be able to switch between players
+    # get it to work with one player and go from there
     @player = Player.new
   end
 end
