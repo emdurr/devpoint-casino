6664571eb438d77024a4b0d44bb1d88f760ac2ea
Justin <woodmansee1207@gmail.com>
2016-08-09 15:45:17 -0600

    Update blackjack

--- blackjack.rb
+++ blackjack.rb
@@ -17,16 +17,39 @@ class BlackJack
 
   def dealer_hand
     @deck = Deck.new
-    puts "Dealer's starting hand is #{@dealers_first_card.find_rank} of #{@dealers_first_card.suit} "
     @dealers_first_card = @deck.return_a_card
-    @dealsers_second_card = @deck.return_a_card
+    @dealers_second_card = @deck.return_a_card
+    puts "Dealer's starting hand is \n#{@dealers_first_card.find_rank} of #{@dealers_first_card.suit} "
+    puts "Card upside down."
+    player_hand
   end
 
   def player_hand
-    puts "#{@player_first_card.find_rank} of #{@player_first_card.suit}"
-    puts "#{@player_second_card.find_rank} of #{@player_second_card.suit}"
     @player_first_card = @deck.return_a_card
     @player_second_card = @deck.return_a_card
+    puts "Your hand is.."
+    puts "#{@player_first_card.find_rank} of #{@player_first_card.suit}"
+    puts "#{@player_second_card.find_rank} of #{@player_second_card.suit}"
+    option
+  end
+
+  def option
+    @player_third_card = @deck.return_a_card
+    puts '(H)it'
+    puts '(S)tay'
+    option_input = gets.strip
+    case option_input
+      when 'H'
+        puts "Card is \n#{@player_third_card.find_rank} of #{@player_third_card.suit}"
+        option
+      else 'S'
+        dealer_option
+    end
+  end
+
+  def dealer_option
+    binding.pry
+    puts "Second card is \n#{@dealers_second_card.find_rank} of #{@dealers_second_card.suit}"
   end
 end
 BlackJack.new(@player)
